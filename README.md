fde5dd052e24ef6318fa0afa1901a40ed8e06ae0
[Gio] <[sigronline@gmail.com]>
2016-09-25 12:11:32 -0500

    basic vr setup

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# vr-beats
+###Node Express HTML Template
+--------------------------
+
+**Description**
+
+This is a simple boilerplate to quick start a Node application with Express and HTML templates.
+
+**Use**
+ - Clone or download this repo
+ - Run `npm install`
+ - Run `npm start`
+ - Go to http://localhost:3000
+
+**NPM Packages used**
+ - HTML as template rendering engine through http://ejs.co/
+ - Watch changes to the files through http://nodemon.io/ 
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// set the /views folder as default for templates
+app.set('views', __dirname + '/views');
+// set HTML as template rendering engine through http://ejs.co/
+app.engine('html', require('ejs').renderFile);
+
+// set the /public folder to access files on client side
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set the / root to access for the index.html file
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
+
+// you can set additional routes to any html file
+// app.get('/about', function (req, res) {
+//   res.render('about.html');
+// });
+
+// to watch changes to the files use 'npm start' => http://nodemon.io/ 
+app.listen(7357, function () {
+  console.log('They see me running... they listening on port 3000!', Date() );
+});
